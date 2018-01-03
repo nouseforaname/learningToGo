6cafe3875022ea610dc6dcae1aacca5418f916bb
nouse <nouse@DESKTOP-HALBFEG>
2018-01-03 17:12:21 +0100

    did some more gruntwork

--- src/euler/sum.go
+++ src/euler/sum.go
@@ -1,19 +1,89 @@
 package euler
 
 import (
-
+	"math"
+	"reflect"
 )
 
+type Prime int
+
+func (p *Prime) checkPrime(primes []int64) bool {
+	isPrime := true
+	intPrime := reflect.ValueOf(*p).Int()
+	primeTestLimit := math.Sqrt(float64(intPrime))
+	for _, confirmedPrime := range primes {
+		if intPrime%confirmedPrime == 0 {
+			isPrime = false
+			break
+		} else if float64(confirmedPrime) < primeTestLimit {
+			break
+		}
+	}
+	return isPrime
+}
+
 // calculates the sum of the multiples of a number up to limit
-func AddMultiples(numbers []int, limit int) int{
-	sum:=0;
-	for i:=1; i<=limit; i++ {
-		for _,number:=range numbers{
-			if (i%number) == 0 {
-				sum+=i
+func AddMultiples(numbers []int, limit int) int {
+	sum := 0
+	for i := 1; i <= limit; i++ {
+		for _, number := range numbers {
+			if (i % number) == 0 {
+				sum += i
 				break
 			}
 		}
 	}
 	return sum
+}
+
+//calculates the sum of even, uneven or all numbers up to limit. set even to nil to sum even and uneven numbers
+func FibonacciSum(limit int, even interface{}) int {
+	sum := 0
+	cache := 1
+	for current := 1; current < limit; {
+		switch even.(type) {
+		case bool:
+			if even.(bool) && current%2 == 0 {
+				sum += current
+			} else if !even.(bool) && current%2 != 0 {
+				sum += current
+			}
+		default:
+			sum += current
+		}
+		cache, current = current, (cache + current)
+
+	}
+	return sum
+}
+
+//the solution is not optimal but i can play around with interfaces, methods and pointers.. probably a closure with an anonymous
+//function will be better for calculating primes. i dont know, im learning. so lets call this the way i currently understand
+func BiggestPrimeFactor(number int) int64 {
+	rest := int64(number)
+	var primes []int64
+	primes = make([]int64, 0)
+	primes = append(primes, 2)
+	factors := make([]int64, 0)
+	limit := int64(math.Sqrt(float64(number)))
+	for i := int64(3); i <= limit; i += 2 {
+		p := Prime(i)
+		isPrime := p.checkPrime(primes)
+		if isPrime {
+			primes = append(primes, int64(i))
+			for _, v := range primes {
+				if int64(rest)%v == 0 {
+					rest = rest / v
+					factors = append(factors, v)
+					if rest == 1 {
+						return v
+					}
+				}
+			}
+		}
+		if i >= limit-1 {
+			return rest
+		}
+	}
+	return 0
 }
--- src/helloworld/main.go
+++ src/helloworld/main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-	fmt.Println("hello new")
-	numbersToCalc:= []int{3,5}
-	fmt.Println(euler.AddMultiples(numbersToCalc, 999))
+	fmt.Println("hello changed")
+	//Problem 1
+	//fmt.Println(euler.AddMultiples([]int{3,5}, 99))
+	//Problem 2
+	//fmt.Println(euler.FibonacciSum(4000000, false))
 	
+	fmt.Println(euler.BiggestPrimeFactor(600851475143))
 }
